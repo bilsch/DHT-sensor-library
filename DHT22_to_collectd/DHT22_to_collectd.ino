cbbc36129ef737cfac4e177a0f0cbd5328ef196a
Bill Schwanitz <bilsch@bilsch.org>
2013-01-14 07:30:02 -0500

    Updated to work with remote sensors over xbee with instance labeling

--- DHT22_to_collectd/DHT22_to_collectd.ino
+++ DHT22_to_collectd/DHT22_to_collectd.ino
@@ -4,24 +4,13 @@
 #include "DHT.h"
 
 #define DHTPIN 11     // what pin we're connected to
-
-// Uncomment whatever type you're using!
-//#define DHTTYPE DHT11   // DHT 11 
-#define DHTTYPE DHT22   // DHT 22  (AM2302)
-//#define DHTTYPE DHT21   // DHT 21 (AM2301)
-
-// Connect pin 1 (on the left) of the sensor to +5V
-// Connect pin 2 of the sensor to whatever your DHTPIN is
-// Connect pin 4 (on the right) of the sensor to GROUND
-// Connect a 10K resistor from pin 2 (data) to pin 1 (power) of the sensor
+#define DHTTYPE DHT22 // DHT 22  (AM2302)
+String nodeid = "RED";
 
 DHT dht(DHTPIN, DHTTYPE);
 
 void setup() {
   Serial.begin(115200); 
-  // Serial1.begin(115200); // for sending to the pi
-  //  Serial.println("DHTxx test!");
-
   dht.begin();
 }
 
@@ -37,21 +26,15 @@ void loop() {
     // Serial.println("Failed to read from DHT");
   } 
   else {
-    /* 
-     Serial.print("Humidity: "); 
-     Serial.print(h);
-     Serial.print(" %\t");
-     Serial.print("Temperature: "); 
-     Serial.print(t);
-     Serial.println(" *C");
-     */
-    // write to the pi too
+    Serial.print(nodeid);
+    Serial.print(" ");
     Serial.print(h);
     Serial.print(" ");
     Serial.print(t);
     Serial.print("\r\n");
   }
-  delay(10000); 
+  delay(100); 
 }
+
 
 
--- dht2302_serial.rb
+++ dht2302_serial.rb
@@ -3,28 +3,29 @@
 require 'serialport'
 require 'syslog'
 
-debug=true
+debug=false
 
-sp = SerialPort.new "/dev/ttyAMA0", 115200
+sp = SerialPort.new "/dev/ttyUSB1", 115200
 hostname="raspberrypi"
 Syslog.open( 'dht2302_serial', Syslog::LOG_PID, Syslog::LOG_DAEMON | Syslog::LOG_DAEMON)
 
 while 1
   stats = sp.gets.chomp.split()
   if stats.size > 1
-    temp_c   = stats[1]
+    temp_c   = stats[2]
     temp_f   = ( temp_c.to_f * 9 / 5 ) + 32
-    humidity = stats[0]
+    humidity = stats[1]
+    location = stats[0]
 
-    puts "PUTVAL \"#{hostname}/sensor-basement/gauge-tempc\" interval=10 N:#{temp_c}"
-    puts "PUTVAL \"#{hostname}/sensor-basement/gauge-tempf\" interval=10 N:#{temp_f}"
-    puts "PUTVAL \"#{hostname}/sensor-basement/gauge-humidity\" interval=10 N:#{humidity}"
+    puts "PUTVAL \"#{hostname}/sensor-#{location}/gauge-tempc\" interval=1 N:#{temp_c}"
+    puts "PUTVAL \"#{hostname}/sensor-#{location}/gauge-tempf\" interval=1 N:#{temp_f}"
+    puts "PUTVAL \"#{hostname}/sensor-#{location}/gauge-humidity\" interval=1 N:#{humidity}"
     STDOUT.flush
     if debug 
       Syslog.log(Syslog::LOG_INFO, "temp_f: #{temp_f} temp_c: #{temp_c} humidity: #{humidity}")
-      Syslog.log(Syslog::LOG_INFO, "PUTVAL \"#{hostname}/sensor-basement/gauge-tempc\" interval=10 N:#{temp_c}")
-      Syslog.log(Syslog::LOG_INFO, "PUTVAL \"#{hostname}/sensor-basement/gauge-tempf\" interval=10 N:#{temp_f}")
-      Syslog.log(Syslog::LOG_INFO, "PUTVAL \"#{hostname}/sensor-basement/gauge-humidity\" interval=10 N:#{humidity}")
+      Syslog.log(Syslog::LOG_INFO, "PUTVAL \"#{hostname}/sensor-#{location}/gauge-tempc\" interval=1 N:#{temp_c}")
+      Syslog.log(Syslog::LOG_INFO, "PUTVAL \"#{hostname}/sensor-#{location}/gauge-tempf\" interval=1 N:#{temp_f}")
+      Syslog.log(Syslog::LOG_INFO, "PUTVAL \"#{hostname}/sensor-#{location}/gauge-humidity\" interval=1 N:#{humidity}")
     end
   else
     Syslog.log(Syslog::LOG_ERR, "Odd input - #{stats.to_s}")
